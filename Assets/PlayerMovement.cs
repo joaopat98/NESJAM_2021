99b3527622b1f26dc2c5b700ffbb8a48fe9804d6
João Patrício <joaopat98@gmail.com>
2021-03-19 23:42:54 +0000

    Refactor input code

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,31 +5,88 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float inputMovement;
-    bool inputJump;
-    [SerializeField] float speed;
+    struct MovementInput
+    {
+        public float Horizontal;
+        public bool StartJump;
+        public bool EndJump;
+    }
+    MovementInput input = new MovementInput();
     CharacterController2D controller;
+
+    [Header("Movement")]
+    public float Speed = 5;
+    float VerticalVelocity;
+    [Header("Jumping")]
+    [SerializeField] float JumpHeight = 2;
+    [SerializeField] float GravityScale = 1;
+    [SerializeField] float FallMultiplier = 2;
+
     // Start is called before the first frame update
     void Start()
     {
-        //it owrked :)
         controller = GetComponent<CharacterController2D>();
     }
 
 
-    void FixedUpdate()
+    void Update()
     {
-        controller.Move(speed * inputMovement, false, inputJump);
+        Vector3 moveVel = Vector3.zero;
+
+        moveVel += input.Horizontal * Speed * Vector3.right;
+
+        if (VerticalVelocity < 0)
+        {
+            VerticalVelocity += Physics.gravity.y * GravityScale * FallMultiplier * Time.deltaTime;
+        }
+        else
+        {
+            VerticalVelocity += Physics.gravity.y * GravityScale * Time.deltaTime;
+        }
+
+        if (controller.isGrounded)
+        {
+            VerticalVelocity = Physics.gravity.y * Time.deltaTime;
+        }
+
+        if (VerticalVelocity > 0 && !input.EndJump)
+        {
+            VerticalVelocity = 0;
+        }
+
+        if (controller.isGrounded && input.StartJump)
+        {
+            VerticalVelocity = Mathf.Sqrt(-JumpHeight * 2 * Physics.gravity.y * GravityScale);
+        }
+
+        moveVel += VerticalVelocity * Vector3.up;
+
+        controller.Move(moveVel * Time.deltaTime);
+    }
+
+    void LateUpdate()
+    {
+        input.StartJump = false;
+        input.EndJump = false;
     }
 
     public void OnMove(InputValue value)
     {
-        inputMovement = value.Get<Vector2>().x;
+        input.Horizontal = value.Get<Vector2>().x;
     }
 
+    bool previousJump;
     public void OnJump(InputValue value)
     {
-        inputJump = value.Get<int>() == 1;
-        Debug.Log(inputJump);
+        bool currentJump = value.Get<float>() == 1;
+        if (!previousJump && currentJump)
+        {
+            input.StartJump = true;
+        }
+        if (previousJump && !currentJump)
+        {
+            input.EndJump = true;
+        }
+        previousJump = currentJump;
     }
 }
