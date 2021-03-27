da4d2dd773e4c5e2de70174b9798a92c950748ed
Carlos Marques <catralitos11@gmail.com>
2021-03-27 13:08:17 +0000

    Created code skeleton for BouncyCannon enemy

---
+++ Assets/Scripts/Enemies/BouncyCannon/BouncyCannonIdle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class BouncyCannonIdle : BouncyCannonState
+{
+    public static BouncyCannonIdle Create(BouncyCannon target)
+    {
+        BouncyCannonIdle state = BouncyCannonState.Create<BouncyCannonIdle>(target);
+        return state;
+    }
+
+    public override void StateStart()
+    {
+        base.StateStart();
+        target.cooldownLeft = target.shootingCooldown;
+        //TODO trocar sprite
+    }
+
+    public override void StateUpdate()
+    {
+        target.cooldownLeft -= Time.deltaTime;
+        if (target.cooldownLeft <= 0)
+        {
+            SetState(BouncyCannonShoot.Create(target));
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/BouncyCannon/BouncyCannonState.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public abstract class BouncyCannonState : EnemyState<BouncyCannon>
+{
+    new protected SpriteRenderer renderer;
+    protected static new T Create<T>(BouncyCannon target) where T : BouncyCannonState
+    {
+        var state = EnemyState<BouncyCannon>.Create<T>(target);
+        state.renderer = target.GetComponent<SpriteRenderer>();
+        return state;
+    }
+}
