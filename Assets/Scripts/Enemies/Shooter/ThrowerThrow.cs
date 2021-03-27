ea9da1c17eb43bbc7a437d9e346f94fcd282e968
Carlos Marques <catralitos11@gmail.com>
2021-03-27 11:45:11 +0000

    Made the skeleton code for the thrower enemy

---
+++ Assets/Scripts/Enemies/Shooter/ThrowerThrow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ThrowerThrow : ThrowerState
+{
+    public static ThrowerThrow Create(Thrower target)
+    {
+        ThrowerThrow state = ThrowerState.Create<ThrowerThrow>(target);
+        return state;
+    }
+
+    public override void StateStart()
+    {
+        base.StateStart();
+        //TODO trocar sprite/animação
+    }
+
+    public override void StateUpdate()
+    {
+        //TODO atirar uma bomba
+        SetState(ThrowerIdle.Create(target));
+    }
+}
