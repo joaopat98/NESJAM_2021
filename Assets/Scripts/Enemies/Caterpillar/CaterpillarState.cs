555f9ae3427c9ce2df00c9d32306e26f99c21ed7
Carlos Marques <catralitos11@gmail.com>
2021-03-27 01:41:45 +0000

    Criei o esqueleto para o inimigo Caterpilar

---
+++ Assets/Scripts/Enemies/Caterpillar/CaterpillarState.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public abstract class CaterpillarState : EnemyState<Caterpillar>
+{
+    protected Animator animator;
+    protected static new T Create<T>(Caterpillar target) where T : CaterpillarState
+    {
+        var state = EnemyState<Caterpillar>.Create<T>(target);
+        state.animator = target.GetComponent<Animator>();
+        return state;
+    }
+}
