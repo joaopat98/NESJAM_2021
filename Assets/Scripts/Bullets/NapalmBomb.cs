5a035ca7c5bcb39d5f4fc66eae15c8600bf17a05
Carlos Marques <catralitos11@gmail.com>
2021-03-27 23:19:25 +0000

    Napalm Shot (scripts and bullet prefabs) implemented

---
+++ Assets/Scripts/Bullets/NapalmBomb.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class NapalmBomb: Bullet
+{
+    public float detonationTime = 2f;
+    public GameObject bulletPrefab;
+
+    private void Start()
+    {
+        Invoke("Detonate", detonationTime);
+    }
+
+    void Detonate()
+    {
+        //TODO play explosion animation
+        var bullet1 = Instantiate(bulletPrefab, transform.position, Quaternion.identity).GetComponent<Bullet>();
+        var bullet2 = Instantiate(bulletPrefab, transform.position, Quaternion.identity).GetComponent<Bullet>();
+        var bullet3 = Instantiate(bulletPrefab, transform.position, Quaternion.identity).GetComponent<Bullet>();
+        var bullet4 = Instantiate(bulletPrefab, transform.position, Quaternion.identity).GetComponent<Bullet>();
+        bullet1.Init(new Vector2(-1, -1));
+        bullet2.Init(new Vector2(1, -1));
+        bullet3.Init(new Vector2(-0.5f, -1));
+        bullet4.Init(new Vector2(0.5f, -1));
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Player/Weapons/NapalmShot/NapalmShot.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NapalmShot : Weapon
+{
+    bool FireStarted;
+    bool FireEnded;
+    bool previousFire = false;
+    [SerializeField] GameObject BulletPrefab;
+    [SerializeField] Transform BulletSpawn;
+
+    void Awake()
+    {
+    }
+
+    void Update()
+    {
+        AddTimeSinceLastFire(Time.deltaTime);
+        //On Key up Fire
+        if (FireEnded)
+        {
+            FireBullet();
+        }
+    }
+
+    private void FireBullet()
+    {
+        var bullet = Instantiate(BulletPrefab, BulletSpawn.position, Quaternion.identity).GetComponent<Bullet>();
+        bullet.Init(Vector3.up);
+    }
+
+    void LateUpdate()
+    {
+        FireStarted = false;
+        FireEnded = false;
+    }
+
+    protected override void OnFire(InputValue value)
+    {
+        bool currentFire = value.Get<float>() == 1;
+        if (!previousFire && currentFire)
+        {
+            if (GetReadyToFire())
+            {
+                FireStarted = true;
+                ResetTimeSinceLastFire();
+            }
+        }
+        if (previousFire && !currentFire)
+        {
+            if (GetAbleToFire())
+            {
+                FireEnded = true;
+            }
+        }
+        previousFire = currentFire;
+    }
+}
