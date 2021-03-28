ad2f47ebf6ef21af0542bf6faedddbbcc7dc401f
Fowlron <pedromfgodinho@gmail.com>
2021-03-28 19:44:04 +0100

    Added weapon icon

---
+++ Assets/Scripts/UI/WeaponUI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class WeaponUI : MonoBehaviour
+{
+    private PlayerEntity player;
+    private Image image;
+    private Weapon lastWeapon;
+
+    void Start()
+    {
+        player = PlayerEntity.instance;
+        image = gameObject.GetComponent<Image>();
+        lastWeapon = player.weapons.GetCurrentSet().GetCurrentWeapon();
+    }
+
+    void Update()
+    {
+        if (lastWeapon != player.weapons.GetCurrentSet().GetCurrentWeapon())
+        {
+            lastWeapon = player.weapons.GetCurrentSet().GetCurrentWeapon();
+            image.sprite = lastWeapon.icon;
+        }
+    }
+}
