a22eff7843950a08062cf7dbe438c8e81a0dfd1b
João Patrício <joaopat98@gmail.com>
2021-03-28 19:19:17 +0100

    Reorganize boss

--- Assets/Scripts/Enemies/Boss/SecondPhase/Light/BossSecondPhaseLightState.cs
+++ Assets/Scripts/Enemies/Boss/SecondPhase/Light/BossSecondPhaseLightState.cs
@@ -3,20 +3,22 @@ using UnityEngine;
 
 public abstract class BossSecondPhaseLightState : BossState
 {
-    protected Boss.Light.Second props;
+    protected Boss.Second phaseProps;
+    protected Boss.Second.Light props;
     public static new T Create<T>(Boss target) where T : BossSecondPhaseLightState
     {
         var state = BossState.Create<T>(target);
-        state.props = target.light.second;
+        state.phaseProps = target.second;
+        state.props = target.second.light;
         return state;
     }
 
     public override void OnGetHit()
     {
         base.OnGetHit();
-        if (target.CurrentHealth <= props.HealthThreshold)
+        if (target.CurrentHealth <= phaseProps.HealthThreshold)
         {
-            props.Assets.SetActive(false);
+            phaseProps.Assets.SetActive(false);
             SetState(BossThirdPhaseIdle.Create(target));
         }
     }
